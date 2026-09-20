028941399e6e6739e2158c97071a5625c5a27494
monsterhands <beinean.evely@gmail.com>
2026-09-20 15:26:00 -0400

    Task 1 parts a and b attempted and completed

--- Evely_Bei_Motion2026/Assets/Scripts/Controllers/Player.cs
+++ Evely_Bei_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -10,8 +10,39 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public Vector3 bombOffset;
+    public float bombTrailSpacing;
+    public int numberOfTrailBombs;
+
     void Update()
     {
+        if(Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            float offsetDistance = 0.5f;
+            bombOffset = transform.position * offsetDistance;
+            SpawnBombAtOffset(bombOffset);
+        }
 
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            bombTrailSpacing = 0.8f;
+            numberOfTrailBombs = 3;
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
+    }
+
+    public void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, inOffset, Quaternion.identity);
+    }
+
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+        Vector3 bombPosition = transform.position;
+        for(int i = 0; i < inNumberOfBombs; i++)
+        {
+            bombPosition.y -= inBombSpacing;
+            Instantiate(bombPrefab, bombPosition, Quaternion.identity);
+        }        
     }
 }
